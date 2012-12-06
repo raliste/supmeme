5a61942d9a067f3f6b9f635733f79c38c3bc0586
Rodrigo Aliste P <rodrigo@joldit.com>
2012-12-06 02:00:46 -0300

    Adds a simple scoring algorithm.

--- frontend/views.py
+++ frontend/views.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
 
 import pymongo
+import datetime
 
 
 DB_NAME = 'supmeme2'
+
+
 _connection = []
 def get_connection():
   if not _connection:
@@ -12,9 +15,17 @@ def get_connection():
 
 
 app = Flask(__name__)
-app.debug = True
 
 @app.route('/')
 def index():
   c = get_connection()
-  return render_template('index.html', entries=c.entries.find())
+  now = datetime.datetime.now()
+
+  def compute_score(entry, points=1, gravity=1.8):
+    delta = now - entry['published_at']
+    hours_since = delta.days * 24 + delta.seconds / 3600.0
+    return points / ((hours_since + 2) ** gravity)
+
+  entries = [(compute_score(entry), entry) for entry in c.entries.find()]
+  sorted_entries = sorted(entries, key=lambda s: s[0], reverse=True)
+  return render_template('index.html', entries=sorted_entries)
